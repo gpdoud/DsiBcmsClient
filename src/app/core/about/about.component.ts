4174a025f8405e92d0739f871927cf2981f0bddb
Greg Doud <gpdoud@gmail.com>
2020-02-21 07:40:46 -0500

    Finished About

--- src/app/core/about/about.component.ts
+++ src/app/core/about/about.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ConfigService } from '@feat/config/config.service';
+import { Config } from '@feat/config/config.class';
 
 @Component({
   selector: 'app-about',
@@ -7,9 +9,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AboutComponent implements OnInit {
 
-  constructor() { }
+  author: string;
+  copyright: string;
+  status: string;
+  version: string;
+  
+  constructor(
+    private cfg: ConfigService
+  ) { }
 
   ngOnInit() {
+    this.cfg.getKeys("author,copyright,status,version").subscribe(
+      res => {
+        for(let config of res) {
+          switch(config.keyValue) {
+            case "author": this.author = config.dataValue; break;
+            case "copyright": this.copyright = config.dataValue; break;
+            case "status": this.status = config.dataValue; break;
+            case "version": this.version = config.dataValue; break;
+
+          }
+        }
+      }
+    );
   }
 
 }
