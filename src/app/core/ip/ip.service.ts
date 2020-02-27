8eb9b06c64a3f5480adfdaeaa0a85745214830a9
Greg Doud <gpdoud@gmail.com>
2020-02-27 07:44:55 -0500

    Corrected search of valid ips

--- src/app/core/ip/ip.service.ts
+++ src/app/core/ip/ip.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { LoggerService } from '@core/logger/logger.service';
 import { Ip } from './ip.class';
+import { AppInitService } from 'app/app-init.service';
 
 const DoudIp: string = "69.133.52.201";
 const DoudPhoneIp: string = "174.233.133.93";
@@ -17,17 +18,27 @@ export class IpService {
 
   isValidDomain: boolean;
 
+  setIsValidIp(myIp: string): void {
+    let validIps = this.init.config.validIps;
+    this.isValidDomain = false;
+    for(let validIp of validIps) {
+      this.isValidDomain = this.isValidDomain || validIp.ip.startsWith(myIp)
+    }
+  }
+
   constructor(
+    private init: AppInitService,
     private http: HttpClient,
     private log: LoggerService
   ) {
     this.http.get(GetIpUrl).subscribe(
       (res: Ip) => {
         let myIp = res.ip;
-        this.isValidDomain = myIp.includes(DoudIp) 
-                            // || myIp.includes(DoudPhoneIp)
-                            || myIp.includes(KenIP)
-                            || myIp.includes(MaxIp);
+        this.setIsValidIp(myIp);
+        // this.isValidDomain = myIp.includes(DoudIp) 
+        //                     // || myIp.includes(DoudPhoneIp)
+        //                     || myIp.includes(KenIP)
+        //                     || myIp.includes(MaxIp);
         this.log.debug(`IP: ${myIp}`)
       }
     );
