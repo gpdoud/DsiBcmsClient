238fd0615cc5a780d1385df9c8ce2426bb16f9d8
Greg Doud <gpdoud@gmail.com>
2020-05-06 16:01:22 -0400

    Fixed Commentary-list and user class
    * Fixed commentary list for student
    * Added styles for Commentator and Actions
    * Changed user commentaries to studentCommentaries

--- src/app/feat/user/user.class.ts
+++ src/app/feat/user/user.class.ts
@@ -18,7 +18,7 @@ export class User {
     updated: string;
     role: Role;
     roleName: string;
-    commentaries: Commentary[] = [];
+    studentCommentaries: Commentary[] = [];
 
     constructor() {}
 }
