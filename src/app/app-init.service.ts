ad4276258ae9f6286bba6e1af2b8b6679826d8a2
Greg Doud <gpdoud@gmail.com>
2023-07-26 16:09:57 -0400

    Set path to the assets/config.json
    * Had to create a path in Azure

--- src/app/app-init.service.ts
+++ src/app/app-init.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { LoggerService } from './core/logger/logger.service';
+import { UserService } from '@feat/user/user.service';
 
 const baseUrl = "http://localhost:5000/dsi";
 // const baseUrl = "https://localhost:44379/dsi";
@@ -14,12 +15,12 @@ export class AppInitService {
 
   config: any;
   getSettings(): Promise<any> {
-    return this.http.get("./assets/config.json").toPromise().then(
+    return this.http.get("/assets/config.json").toPromise().then(
       (data: any) => {
         this.config = data;
         this.logger.debug("Init Config:", this.config);
       }
-    )
+    );
   }
 
 
